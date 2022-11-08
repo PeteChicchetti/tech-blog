9369efb297d224901345ae8461b6a8797c3abd52
PeteChicchetti <pj0524@gmail.com>
2022-11-08 17:57:48 -0500

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+// Require
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Class Model
+class Post extends Model {}
+
+Post.init(
+    // Model
+    {
+        post_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    // DataTypes
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: false,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+// Export model
+module.exports = Post;
