a553941763f8a1afb069098cb5f63758a6d9d888
PeteChicchetti <pj0524@gmail.com>
2022-11-08 18:14:17 -0500

    added seeds/index run function and json files for seedData

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+// Require
+const sequelize = require('../config/connection');
+
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+const commentSeedData = require('./commentSeedData.json');
+const postSeedData = require('./postSeedData.json');
+const userSeedData = require('./userSeedData.json');
+
+// Async function to seed the Database
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    // Bulkcreate the seed data for each model 
+    await Comment.bulkCreate(commentSeedData);
+    await Post.bulkCreate(postSeedData);
+    await User.bulkCreate(userSeedData);
+
+  process.exit(0);
+};
+
+seedDatabase();
