69c4a5bdaa768e1a704b25257ad19c0b0d9eead3
PeteChicchetti <pj0524@gmail.com>
2022-11-09 18:38:11 -0500

    added session info, require controllers and SequelizeStore

--- server.js
+++ server.js
@@ -5,20 +5,46 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+// Import connection to DB
+const sequelize = require('./config/connection');
+// Initializes Sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Import connection to DB
-const sequelize = require('./config/connection');
-// 
-// 
-// 
-// 
-// 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  // Custom cookie
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'))
 
 // Connect to the database before starting the Express.js server
 sequelize.sync({ force: false }).then(() => {
