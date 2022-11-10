d37aff0bc4db49f292a7c5e88f6b0a8abddff991
PeteChicchetti <pj0524@gmail.com>
2022-11-10 17:28:29 -0500

    added model index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,33 @@
-//Post belongs to User
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment')
 
-//Post has many Comments
+// Post belongs to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+// Comment belongs to User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+// Post has many Comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+// User has many Post
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+// User has many Comment
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
-//Comment belongs to User
+module.exports = { User, Post, Comment };
