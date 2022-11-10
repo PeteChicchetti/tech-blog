37b6e10c3549f883daf3b628370a9895a96dabfb
PeteChicchetti <pj0524@gmail.com>
2022-11-10 16:49:21 -0500

    added bcrypt to user

--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 // Require
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // Class Model
-class User extends Model {}
+class User extends Model {
+    // Check if the login password matches the saved password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
 
 User.init(
     // Model
@@ -26,8 +32,22 @@ User.init(
             },
         },
     },
+    
     // DataTypes
     {
+        // Hooks
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+              if (updatedUserData.password) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              }
+              return updatedUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: false,
