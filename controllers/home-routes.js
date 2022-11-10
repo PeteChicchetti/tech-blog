d3f1e892f4e4fc5358e2eb2f67ae38257bf1e88b
PeteChicchetti <pj0524@gmail.com>
2022-11-09 20:23:57 -0500

    added partials folder

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,13 @@ const withAuth = require('../utils/auth');
 
 // Get route for homepage
 router.get('/', withAuth, async (req, res) => {
-    
+      // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/main');
+    return;
+  }
+
+  res.render('signup');
 });
 
 // Get route for posts
